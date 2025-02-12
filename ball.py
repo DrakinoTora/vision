54a464e0bc3174f6b6263e1c4e76948e92647539
DrakinoTora <drakino.tora@gmail.com>
2025-02-12 21:10:37 +0700

    Update ball.py

--- ball.py
+++ ball.py
@@ -4,7 +4,7 @@ import numpy as np
 def ball(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_orange = np.array([1, 120, 75])
+    lower_orange = np.array([1, 80, 20])
     upper_orange = np.array([25, 255, 255])
 
     mask_ball = cv2.inRange(hsv, lower_orange, upper_orange)
@@ -25,12 +25,14 @@ def field(frame):
     green_mask2 = cv2.dilate(green_mask1, kernel, iterations=6)
 
     contours, _ = cv2.findContours(green_mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-       
+    
+    if not contours:
+        return frame, np.zeros_like(green_mask2)
+
     largest_contour = max(contours, key=cv2.contourArea)
     
     hull = cv2.convexHull(largest_contour)
 
-    # Buat mask untuk ROI
     mask = np.zeros_like(green_mask2)
     cv2.drawContours(mask, [hull], -1, 255, thickness=-1)  # Isi area dalam hull
     
@@ -48,7 +50,7 @@ def detect(mask_ball, mask_field, frame):
         minDist=50,
         param1=50,
         param2=30,
-        minRadius=10,
+        minRadius=1,
         maxRadius=1000
     )
 
@@ -57,7 +59,7 @@ def detect(mask_ball, mask_field, frame):
         for i in circles[0, :]:
             x, y, r = i[0], i[1], i[2]
 
-            #horizontal line
+        
             line_y = y
             line_x_start = 0
             line_x_end = frame.shape[1]
@@ -87,7 +89,6 @@ def detect(mask_ball, mask_field, frame):
             r_new = int(total_x_pixel/2)
             x_new = first_orange + int(total_x_pixel/2)
 
-            #vertiksl line
             line_x = first_orange + int(r_new /4)
             line_y_start = 0
             line_y_end = frame.shape[0]
@@ -97,7 +98,6 @@ def detect(mask_ball, mask_field, frame):
             top_orange = frame.shape[0]
             bot_orange = 0
 
-            # Mencari indeks pertama dan terakhir dari piksel oranye
             orange_col=0   #banyak pixel bola berturut turut
             void_col=0     #banyak pixel non bola berturut turut
             for idy in range(orange_vline.size - 1, -1, -1):
@@ -119,7 +119,6 @@ def detect(mask_ball, mask_field, frame):
 
             y_new= bot_orange - int(total_y_pixel/2)
 
-            #horizontal line
             if y_new != y:
                 line_y = y_new
                 orange_hline = mask_ball[line_y, line_x_start:line_x_end]
@@ -127,13 +126,11 @@ def detect(mask_ball, mask_field, frame):
                 first_orange = None
                 last_orange = None
 
-                # Mencari indeks pertama dari piksel oranye
                 for idx in range(orange_hline.size):
                     if orange_hline[idx] == 255:
                         first_orange = idx
                         break
 
-                # Mencari indeks terakhir dari piksel oranye
                 for idx in range(orange_hline.size - 1, -1, -1):
                     if orange_hline[idx] == 255:
                         last_orange = idx
@@ -146,7 +143,6 @@ def detect(mask_ball, mask_field, frame):
                 r_new = int(total_x_pixel/2)
                 x_new = first_orange + int(total_x_pixel/2)
 
-            #field ratio
             R = int(r_new * 1.5)  #jarak deteksi
             x1, y1 = max(x_new - R, 0), max(y_new - R, 0)
             x2, y2 = min(x_new + R, frame.shape[1]), min(y_new + R, frame.shape[0])
@@ -157,38 +153,31 @@ def detect(mask_ball, mask_field, frame):
             surrounding_ball = mask_ball[y1:y2, x1:x2]
             ball_ratio = np.sum(surrounding_ball == 255) / surrounding_ball.size
 
-            if (field_ratio > 0.16 and ball_ratio < 0.37):
+            if (field_ratio > 0.16 and ball_ratio < 0.47):
                 cv2.line(frame, (x_new, y_new + r_new), (x_new, y_new - r_new), (0, 255, 0), 2)
                 cv2.line(frame, (x_new - r_new, y_new), (x_new + r_new, y_new), (0, 255, 0), 2)
                 cv2.circle(frame, (x_new, y_new), r_new, (0, 255, 0), 2)
             else:
                 continue
 
-            #distance
             actual_diameter= 0.13 #meter
             focal_length= 71.41
             detected_diameter= total_x_pixel
             if detected_diameter==0:
-                distance=0
+                actual_distance=0
             else:
-                distance= (actual_diameter*focal_length)/detected_diameter
+                actual_distance= (actual_diameter*focal_length)/detected_diameter
+            h_robot= 0
+            distance = np.sqrt(actual_distance**2 - h_robot**2)
             text2 = f"Distance: {distance:.2f} meter"
             cv2.putText(frame, text2, (x_new - r_new, y_new + r_new + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
-            cv2.line(frame, (center_x, y_end), (x_new, y_new), (0, 0, 255), 2)
-            if x_new > center_x:
-                print("bola di kanan %d x" %(x_new - center_x))
-            elif x_new < center_x:
-                print("bola di kiri %d x" %(x_new - center_x))
-            else:
-                print("bola tepat di tengah frame")
-
-            #print("distance= %.3f cm" %(distance*100))
+            cv2.line(frame, (center_x, center_y), (x_new, y_new), (255, 255, 255), 2)
 
     return frame
 
-cap = cv2.VideoCapture(r"D:\GMRT\Altair\vision\Program deteksi bola mp4\sample1.mp4")
-
+cap = cv2.VideoCapture(r"D:\GMRT\Altair\vision\Program deteksi bola mp4\sample2.mp4")
+#cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Error: No Video Opened")
@@ -206,23 +195,29 @@ while True:
     if not ret:
         break
     
+
     height, width = frame.shape[:2]
 
-    # Tentukan posisi X di tengah frame
     center_x = width // 2
+    center_y = height // 2
 
-    # Tentukan posisi Y untuk garis vertikal (dari atas ke bawah)
     y_start = 0
     y_end = height
+    x_start = 0
+    x_end = width
 
     # Gambar garis vertikal di tengah
-    cv2.line(frame, (center_x, y_start), (center_x, y_end), (0, 0, 255), 2)
+    cv2.line(frame, (center_x, y_start), (center_x, y_end), (255, 255, 255), 1)
+    cv2.line(frame, (x_start, center_y), (x_end, center_y), (255, 255, 255), 1)
 
     seg_field, mask_field = field(frame)
     mask_ball = ball(seg_field)
+    #mask_ball = ball(frame)
+    #final_frame = detect(mask_ball, frame, frame)
     final_frame = detect(mask_ball, mask_field, frame)
 
-    #cv2.imshow('lapangan', mask_field)
+    #cv2.imshow('mask field', mask_field)
+    #cv2.imshow('mask ball', mask_ball)
 
     if final_frame is not None:
         #out.write(final_frame)
